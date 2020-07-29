8fe6d0d6b51485f75b872f6dc7d274a6c820c6f3
Massimilian <vasalekmas@gmail.com>
2020-07-29 19:29:12 +0300

    Задание 3, исправления(2)

--- src/ru/progwards/java2/lessons/generics/ArraySort.java
+++ src/ru/progwards/java2/lessons/generics/ArraySort.java
@@ -7,7 +7,7 @@ public class ArraySort {
     public static <T extends Comparable> T[] sort(T... args) {
         for (int i = 0; i < args.length - 1; i++) {
             for (int j = i + 1; j < args.length; j++) {
-                if (args[i].compareTo(args[j]) == -1) { // перепутал право и лево, простите...
+                if (args[i].compareTo(args[j]) > 0) { // перепутал право и лево, простите...
                     T temp = args[j];
                     args[j] = args[i];
                     args[i] = temp;
--- src/ru/progwards/java2/lessons/generics/DynamicArray.java
+++ src/ru/progwards/java2/lessons/generics/DynamicArray.java
@@ -21,21 +21,60 @@ public class DynamicArray<T> {
     public void add(T t) {
         while (ts[count] != null) {
             count++;
-            this.test(count + 1);
+            if (ts.length == count) {
+                this.test(count + 1);
+            }
         }
         this.test(count + 1);
         ts[count++] = t;
     }
 
     public void insert(int pos, T t) {
-        this.test(pos);
+        if (ts.length <= pos) {
+            this.test(pos);
+        } else {
+            this.plusOne(pos);
+        }
         ts[pos] = t;
     }
 
     public void remove(int pos) {
         if (pos <= count) {
-            ts[pos] = null;
+            if (ts.length == 1 && pos == 0) { // во избежание нолевого массива
+                ts[pos] = null;
+            } else {
+                this.minusOne(pos);
+            }
         }
+    }
+
+    private void plusOne(int pos) {
+        T[] tsNew = (T[]) new Object[ts.length + 1];
+        for (int i = 0; i < tsNew.length; i++) {
+            if (i < pos) {
+                tsNew[i] = this.ts[i];
+            } else if (i == pos) {
+                continue;
+            } else {
+                tsNew[i] = this.ts[i - 1];
+            }
+        }
+        ts = tsNew;
+    }
+
+    private void minusOne(int pos) {
+        T[] tsNew = (T[]) new Object[ts.length - 1];
+        for (int i = 0; i < ts.length; i++) {
+            if (i < pos) {
+                tsNew[i] = this.ts[i];
+            } else if (i == pos) {
+                continue;
+            } else {
+                tsNew[i - 1] = this.ts[i];
+            }
+        }
+        ts = tsNew;
+        count = 0;
     }
 
     public T get(int pos) {
