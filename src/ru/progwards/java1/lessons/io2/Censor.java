3c6fb6dd87e7d75abc08d1bd2b10ee22f82adebc
Massimilian <vasalekmas@gmail.com>
2020-11-19 22:42:44 +0300

    Progwards: io2(1)

---
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -0,0 +1,171 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Special class for censoring the text.
+ */
+public class Censor {
+
+    /**
+     * Main static method for censurer's work
+     *
+     * @param inoutFileName the file place
+     * @param obscene       words for censurer
+     * @throws CensorException if something's going wrong
+     */
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
+        Censor censor = new Censor();
+        censor.censor(inoutFileName, obscene);
+    }
+
+    /**
+     * Main non-static method for censurer's work
+     *
+     * @param inoutFileName the file place
+     * @param obscene       words for censore
+     * @throws CensorException if something's going wrong
+     */
+    private void censor(String inoutFileName, String[] obscene) throws CensorException {
+        try {
+            writeFile(inoutFileName, cens(writeFromFile(inoutFileName), obscene, inoutFileName));
+        } catch (IOException e) {
+            throw new CensorException(String.format("%s: %s", inoutFileName, e.getMessage()));
+        }
+    }
+
+    /**
+     * Method to take the text from the file
+     *
+     * @param inoutFileName file place
+     * @return text from file
+     * @throws IOException if something's going wrong
+     */
+    private String writeFromFile(String inoutFileName) throws IOException {
+        FileReader fr = new FileReader(inoutFileName);
+        Scanner scanner = new Scanner(fr);
+        StringBuilder sb = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            sb.append(scanner.nextLine());
+            if (scanner.hasNext()) {
+                sb.append(System.lineSeparator());
+            }
+        }
+        fr.close();
+        return sb.toString();
+    }
+
+    /**
+     * Main method to check words for censore
+     *
+     * @param info    String for censore
+     * @param obscene censoring words
+     * @return censored String
+     */
+    private String cens(String info, String[] obscene, String inOutFileName) throws CensorException {
+        String result = info;
+        for (String s : obscene) {
+            result = replaceInText(result, s, inOutFileName);
+        }
+        return result;
+    }
+
+    /**
+     * Method to censurer word-by-word (with special check at the begining and the end of the String)
+     *
+     * @param result String fo check
+     * @param word   String for change
+     * @return changed String
+     */
+    private String replaceInText(String result, String word, String inOutFileName) throws CensorException {
+        if (word.equals("")) {
+            throw new CensorException(String.format("%s: Impossible word!", inOutFileName));
+        }
+        int point = -1;
+        do {
+            point = result.toLowerCase().indexOf(String.format("%s", word), point + 1);
+            if (point == 0 && result.substring(point, word.length() + 1).toLowerCase().matches(String.format("%s[\\W]", word))) {
+                result = String.format("%s%s", prepareWord(word), result.substring(word.length()));
+            }
+            if (point < result.length() - word.length() && point > 0 && result.substring(point - 1, point + word.length() + 1).toLowerCase().matches(String.format("[\\W]%s[\\W]", word))) {
+                result = String.format("%s%s%s", result.substring(0, point), prepareWord(word), result.substring(point + word.length()));
+            }
+            if (point == result.length() - word.length() && result.substring(point - 1).toLowerCase().matches(String.format("[\\W]%s", word))) {
+                result = String.format("%s%s", result.substring(0, result.length() - word.length()), prepareWord(word));
+            }
+        } while (point != -1);
+        return result;
+    }
+
+    /**
+     * Method for preparing impossible word and putting '*' in it
+     *
+     * @param word to change
+     * @return stars
+     */
+    private String prepareWord(String word) {
+        return "*".repeat(word.length());
+    }
+
+    /**
+     * Method for write prepared text into the file
+     *
+     * @param inoutFileName file place
+     * @param cleaned       text for write
+     * @throws IOException if something's going wrong
+     */
+    private void writeFile(String inoutFileName, String cleaned) throws IOException {
+        FileWriter fw = new FileWriter(inoutFileName);
+        fw.write(cleaned);
+        fw.close();
+    }
+
+    /**
+     * Exception class if something's going wrong
+     */
+    private static class CensorException extends Exception {
+        public CensorException(String s) {
+            super(s);
+        }
+    }
+
+    /**
+     * Delete and create method, specoal for comfort test
+     *
+     * @param file       for create
+     * @param realCreate necessity of creating new file
+     * @throws IOException if something's going wrong
+     */
+    private void forTestDeleteCreate(String file, boolean realCreate) throws IOException {
+        File f = new File(file);
+        if (f.exists()) {
+            f.delete();
+        }
+        if (realCreate) {
+            f.createNewFile();
+        }
+    }
+
+    public static void main(String[] args) throws CensorException, IOException {
+        String file = "text.txt";
+        Censor cens = new Censor();
+        cens.forTestDeleteCreate(file, true);
+        cens.writeFile(file, "Delete dElete DELETE delete! edelete deletee");
+        censorFile(file, new String[]{"delete"});
+        assert cens.writeFromFile(file).equals("****** ****** ****** ******! edelete deletee");
+        cens.forTestDeleteCreate(file, true);
+        cens.writeFile(file, "This is a lISt of \"is\".");
+        censorFile(file, new String[]{"a", "is"});
+        assert cens.writeFromFile(file).equals("This ** * lISt of \"**\".");
+        cens.forTestDeleteCreate(file, true);
+        cens.writeFile(file, "Java — строго типизированный объектно-ориентированный язык программирования, разработанный компанией Sun Microsystems (в последующем приобретённой компанией Oracle).");
+        censorFile(file, new String[]{"Java", "Oracle", "Sun", "Microsystems"});
+        assert cens.writeFromFile(file).equals("**** — строго типизированный объектно-ориентированный язык программирования, разработанный компанией *** ************ (в последующем приобретённой компанией ******).");
+        cens.forTestDeleteCreate(file, false);
+    }
+}
+
--- src/ru/progwards/java1/lessons/io2/Translator.java
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -1,19 +1,40 @@
 package ru.progwards.java1.lessons.io2;
 
+/**
+ * Class for changing words in text
+ */
 public class Translator {
     private final String[] inLang;
     private final String[] outLang;
 
+    /**
+     * Constructor with parameters inLang (entering words) and outLang (escaping words)
+     * @param inLang words for enter
+     * @param outLang words for escape
+     */
     public Translator(String[] inLang, String[] outLang) {
         this.inLang = inLang;
         this.outLang = outLang;
     }
 
+    /**
+     * Method for preparing the array of words and putting into the final sentence
+     * @param sentence for translate
+     * @return changed sentence (String)
+     */
     public String translate(String sentence) {
         String[] words = prepareWords(sentence, true);
         return constructAll(sentence, changeWords(words), words, prepareWords(sentence, false));
     }
 
+    /**
+     * Method for construct final sentence
+     * @param sentence for change
+     * @param changedWords is array with prepared (changed if need) words
+     * @param words is array with only words of sentence
+     * @param punctuations is array with punctuation gifts
+     * @return finally prepared String
+     */
     private String constructAll(String sentence, String[] changedWords, String[] words, String[] punctuations) {
         int wordPos = 0;
         int punctPos = 0;
@@ -33,6 +54,11 @@ public class Translator {
         return sb.toString();
     }
 
+    /**
+     * The method for change the array with words on @changed@ words
+     * @param words old words
+     * @return changed words
+     */
     private String[] changeWords(String[] words) {
         String[] newWords = new String[words.length];
         for (int i = 0; i < words.length; i++) {
@@ -41,6 +67,11 @@ public class Translator {
         return newWords;
     }
 
+    /**
+     * Method for change the word if it need
+     * @param word for change
+     * @return changed (maybe) word
+     */
     private String changeWord(String word) {
         String result = word.toLowerCase();
         for (int i = 0; i < inLang.length; i++) {
@@ -52,6 +83,12 @@ public class Translator {
         return result;
     }
 
+    /**
+     * Method to prepare the words and punctuation arrays
+     * @param sentence with words (of punctuation) for prepare
+     * @param isWords true = words; false = punctuation
+     * @return prepared array with words or punctuation
+     */
     private String[] prepareWords(String sentence, boolean isWords) {
         int pos = 0;
         String[] buffer = new String[sentence.length()];
@@ -67,6 +104,14 @@ public class Translator {
         return result;
     }
 
+    /**
+     * Method for prepare words and punctuation
+     *
+     * @param sentence for work
+     * @param i position into sentence
+     * @param isAlphabetic true if is letter; false if is not.
+     * @return prepared group of symbols for add into arrays
+     */
     private String fillValues(String sentence, int i, boolean isAlphabetic) {
         StringBuilder sb = new StringBuilder();
         if (isAlphabetic) {
@@ -81,6 +126,13 @@ public class Translator {
         return sb.toString();
     }
 
+    /**
+     * Method for check all words with upper letters
+     * @param changedWords array with new words
+     * @param words array with words of sentence
+     * @param wordPos number of position into the sentence
+     * @return
+     */
     private String checkUppers(String[] changedWords, String[] words, int wordPos) {
         String word = words[wordPos];
         String changed = changedWords[wordPos];
