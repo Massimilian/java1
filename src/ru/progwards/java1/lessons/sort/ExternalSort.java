66d6e8062a34eec1d36bb5dae9fb86739b913094
Massimilian <vasalekmas@gmail.com>
2022-01-26 11:38:10 +0300

    Progwards: sort(2)

---
+++ src/ru/progwards/java1/lessons/sort/ExternalSort.java
@@ -0,0 +1,5 @@
+package ru.progwards.java1.lessons.sort;
+
+// см. package java2
+public class ExternalSort {
+}
--- src/ru/progwards/java2/lessons/TestArea/src/main/java/Main.java
+++ src/ru/progwards/java2/lessons/TestArea/src/main/java/Main.java
@@ -1,6 +1,42 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        try (ServerSocket ss = new ServerSocket(80)) { // в качестве аргумента указываем порт
+            Socket serverSocket = ss.accept(); // получаем серверный сокет
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
 
+class RequestHandler implements Runnable {
+
+    Socket server; // сюда мы передаём серверный сокет
+
+    public RequestHandler(Socket server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream is = server.getInputStream();
+            OutputStream os = server.getOutputStream();
+        ) {
+            Scanner sc = new Scanner(is);
+            boolean isDone = false;
+
+            while(!isDone && sc.hasNextLine()) {
+
+            }
+        }catch (IOException io) {
+            io.printStackTrace();
+        }
     }
 }
